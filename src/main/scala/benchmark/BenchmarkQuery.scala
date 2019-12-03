127791b73997481dfd6d1f3b00047617345df73d
omar yousry <omayousry@gmail.com>
2019-12-03 19:24:12 +0200

    Revert "Close spark context after submit"
    This reverts commit 284ab1111c7937183fffa065b81ee90f3e19a43e.

--- src/main/scala/benchmark/BenchmarkQuery.scala
+++ src/main/scala/benchmark/BenchmarkQuery.scala
@@ -2,7 +2,6 @@ package benchmark
 
 import java.sql.{Connection, DriverManager, SQLException}
 
-import com.utilities.SparkBuilder
 import org.apache.spark.sql._
 import org.apache.spark.SparkContext
 
@@ -117,6 +116,5 @@ object BenchmarkQuery {
         s") ON DUPLICATE KEY UPDATE time=${r._2}")
       )
     }
-    SparkBuilder.stop()
   }
 }
